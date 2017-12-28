f22d129a314c4a43796ae9112297ec0459c91d62
Hughes, Michael <hughesm@musc.edu>
2017-12-28 15:12:29 -0500

    Add EntityData abstract class to entity classes

---
+++ EmpHRIS.Entities/Position.cs
@@ -0,0 +1,65 @@
+﻿using HRIS.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace EmpHRIS.Entities
+{
+    public class Position : EntityData
+    {
+        public PosClass posClass { get; set; }
+        public DateTime effDate { get; set; }
+        public string fteNumber { get; set; }
+    }
+
+    public class PosClass : EntityData
+    {
+        public CensusCode censusCode { get; set; }
+        public WorkComp workComp { get; set; }
+        public SalaryStruct salaryStruct { get; set; }
+        public SHACode shaCode { get; set; }
+        public string code { get; set; }
+        public string subclass { get; set; }
+        public DateTime effDate { get; set; }
+        
+    }
+
+  
+    public class SHACode : EntityData
+    {
+        public string code { get; set; }
+    }
+
+    public class WorkComp : EntityData
+    {
+        public string code { get; set; }
+    }
+
+    public class CensusCode : EntityData
+    {
+        public string code { get; set; }
+    }
+
+    public class SalaryStruct : EntityData
+    {
+        public PayGrade payGrade {get;set;}
+        public string code { get; set; }
+       
+
+    }
+
+    public class PayGrade : EntityData
+    {
+        public string code { get; set; }
+
+        [DataType(DataType.Currency)]
+        public double minAmount { get; set; }
+
+        [DataType(DataType.Currency)]
+        public double midAmount { get; set; }
+
+        [DataType(DataType.Currency)]
+        public double maxAmount { get; set; }
+    }
+}
---
+++ HRIS.Interfaces/EntityData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HRIS.Interfaces
+{
+    public abstract class EntityData
+    {
+        public int Id { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime UpdateDate { get; set; }
+        public string CreateBy { get; set; }
+        public string UpdateBy { get; set; }
+    }
+}
